a8e10f924eca0f1b045a8f3ff833de226bfd606d
이준희 <ljh23502@naver.com>
2025-07-16 16:40:43 +0900

    22864번

---
+++ 22864.py
@@ -0,0 +1,14 @@
+A,B,C,M = map(int,input().split())
+piro = 0
+work = 0
+
+for _ in range(24):
+    if piro + A <=10:
+        piro +=A
+        work +=B
+    else:
+        piro -=C
+        if piro < 0:
+            piro = 0
+
+print(work)
